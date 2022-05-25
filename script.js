cdd9ba3b1b2b8ed8a3fc3e00debbea614a571398
Antonius <83505955+Skytone6@users.noreply.github.com>
2022-05-24 21:32:05 -0400

    Add files for Scrimba Project

---
+++ script.js
@@ -0,0 +1,51 @@
+let amount = document.querySelector("#amount").textContent;
+
+let length = () => {
+    let length = document.querySelector("#length");
+    let feet = Math.round((amount * 3.28084) * 1000) / 1000;
+    let meter = Math.round((amount * 0.3048) * 1000) / 1000;
+    length.textContent = `${amount} meters = ${feet} feet | ${amount} feet = ${meter} meters`;
+};
+
+let volume = () => {
+    let volume = document.querySelector("#volume");
+    let gallons = Math.round((amount * 0.2641720524) * 1000) / 1000;
+    let liters = Math.round((amount * 3.78541) * 1000) / 1000;
+    volume.textContent = `${amount} liters = ${gallons} gallons | ${amount} gallons = ${liters} liters`;
+};
+
+let mass = () => {
+    let mass = document.querySelector("#mass");
+    let pounds = Math.round((amount * 2.20462) * 1000) / 1000;
+    let kilos = Math.round((amount * 0.453592) * 1000) / 1000;
+    mass.textContent = `${amount} kilos = ${pounds} pounds | ${amount} pounds = ${kilos} kilos`
+};
+
+length();
+volume();
+mass();
+
+let dark = () => {
+    /*Background Theme CSS*/
+    document.querySelector("#calculations").style.backgroundColor = "rgba(31, 41, 55, 1)";
+    document.querySelector("#calculations").style.color = "white";
+
+    /*Button Change CSS*/
+    document.querySelector("#light").style.background = "white";
+    document.querySelector("#light").style.color = "rgba(31, 41, 55, 1)";
+    document.querySelector("#dark").style.background = "linear-gradient(45deg,#ed4880, #3b69e1)";
+    document.querySelector("#dark").style.color = "rgba(251, 247, 238, 1)";
+};
+
+let light = () => {
+    /*Background Theme CSS*/
+    document.querySelector("#calculations").style.backgroundColor = "rgba(251, 247, 238, 1)";
+    document.querySelector("#calculations").style.color = "black";
+
+    /*Button Change CSS*/
+    document.querySelector("#light").style.background = "linear-gradient(45deg,#ed4880, #3b69e1)";
+    document.querySelector("#light").style.color = "rgba(251, 247, 238, 1)";
+    document.querySelector("#dark").style.background = "black";
+    document.querySelector("#dark").style.color = "rgba(251, 247, 238, 1)";
+};
+
